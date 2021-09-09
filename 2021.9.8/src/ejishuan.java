f7487cd491221ad33489a53dc63ee4257728ee69
汤文顺 <tangwenshun@foxmail.com>
2021-09-09 22:56:37 +0800

    2021.9.8

---
+++ 2021.9.8/src/ejishuan.java
@@ -0,0 +1,5 @@
+public class ejishuan {
+    public static void main(String[] args) {
+        System.out.println(1.0/Math.exp(4.8));
+    }
+}
---
+++ 2021.9.8/src/接口.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+interface A{}
+interface B{}
+interface C1 extends A{}
+interface C2 extends A,B{}
+
+
+public class 接口 {
+    public static void main(String[] args) {
+
+    }
+}
