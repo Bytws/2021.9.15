df2b70f7642e359f9a085eac1db8f8e66e9b892f
Bytws <tangwenshun@foxmail.com>
2021-10-03 02:05:56 +0800

    动物识别系统java

---
+++ 2021.9.15/动物识别系统/src/Facts.java
@@ -0,0 +1,29 @@
+//事实实体
+
+public class Facts {
+
+    private int factNum;
+    private int facts[];
+
+    //初始化
+    Facts(){
+        factNum=0;
+        facts=new int[0];
+    }
+
+    public int getFactNum() {
+        return factNum;
+    }
+    public void setFactNum(int factNum) {
+        this.factNum = factNum;
+    }
+    public int[] getFacts() {
+        return facts;
+    }
+    public void setFacts(int[] facts) {
+        this.facts = facts;
+    }
+
+
+
+}
